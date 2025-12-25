9cbfdf9813cbd2535d12c8394c78b898c5cb436c
Danilpervik <danil_pervik@mail.ru>
2025-12-25 20:59:06 +0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from .parser import parse_lms_log
+from .analyzer import compute_activity_grade_correlation
+from .visualizer import plot_correlations
+
+def analyze_data(filepath: str):
+    df = parse_lms_log(filepath)
+    corr = compute_activity_grade_correlation(df)
+    plot_correlations(corr)
+    return corr
